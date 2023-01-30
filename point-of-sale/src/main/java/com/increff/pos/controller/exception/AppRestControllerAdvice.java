7ada2ae81c71ad653934eeb8ecb91acec5aacfe1
Zeeshan <equbalzeeshan@gmail.com>
2023-01-30 18:35:40 +0530

    Added handling invalid type of fields from the UI

--- point-of-sale/src/main/java/com/increff/pos/controller/exception/AppRestControllerAdvice.java
+++ point-of-sale/src/main/java/com/increff/pos/controller/exception/AppRestControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.increff.pos.controller.exception;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,6 +27,17 @@ public class AppRestControllerAdvice {
 	public MessageData handle(Throwable e) {
 		MessageData data = new MessageData();
 		data.setMessage("An unknown error has occurred - " + e.getMessage());
+		return data;
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public MessageData handleHttpMessageNotReadableException(HttpMessageNotReadableException ex)
+	{
+		JsonMappingException jme = (JsonMappingException) ex.getCause();
+		MessageData data = new MessageData();
+		data.setMessage(jme.getPath().get(0).getFieldName() + " provided is invalid");
 		return data;
 	}
 }
